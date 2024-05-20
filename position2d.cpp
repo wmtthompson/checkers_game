e8aa22c196313c98c77269167e95ab4b0a05c9f0
william <zink2040@yahoo.com>
2024-05-20 18:44:31 -0400

    Added class diagram and added a more legit test, and filled in some more code.

---
+++ position2d.cpp
@@ -0,0 +1,20 @@
+#include "position2d.hpp"
+
+//constructor for this class
+class Position2D
+{
+    private:
+    int x;
+    int y;
+
+    public:
+    void move(Direction dir)
+    {
+        ;
+    }
+
+    void moveTo(Direction dir)
+    {
+        ;
+    }
+};
